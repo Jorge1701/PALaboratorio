3a993e61ed45f25dfc36acae6bf73ea5e462e067
Ale <Ale@192.168.56.1>
2017-08-09 22:21:58 -0300

    Merge origin/Desarrollo into Desarrollo
    Conflicts:
    	PALaboratorio/src/Logica/Artista.java
    	PALaboratorio/src/Logica/DtAlbum.java
    	PALaboratorio/src/Logica/DtAlbumContenido.java
    	PALaboratorio/src/Logica/DtArtista.java
    	PALaboratorio/src/Logica/DtCliente.java
    	PALaboratorio/src/Logica/DtFecha.java
    	PALaboratorio/src/Logica/DtGenero.java
    	PALaboratorio/src/Logica/DtPerfilArtista.java
    	PALaboratorio/src/Logica/DtPerfilCliente.java
    	PALaboratorio/src/Logica/DtPerfilUsuario.java
    	PALaboratorio/src/Logica/DtTema.java
    	PALaboratorio/src/Logica/DtUsuario.java

--- PALaboratorio/src/Logica/Genero.java
+++ PALaboratorio/src/Logica/Genero.java
@@ -1,14 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Logica;
 
-/**
- *
- * @author Ale
- */
+import java.util.HashMap;
+
 class Genero {
+
+    private String nombre;
+    private HashMap<String, Genero> subgeneros;
+    private HashMap<String, Album> albumes;
+    private HashMap<String, ListaDefecto> listasDefecto; 
+
+    public Genero(String nombre, HashMap<String, Genero> subgeneros, HashMap<String, Album> albumes, HashMap<String, ListaDefecto> listasDefecto) {
+        this.nombre = nombre;
+        this.subgeneros = subgeneros;
+        this.albumes = albumes;
+        this.listasDefecto = listasDefecto;
+    }
+
+   
+
+    public void setAlbumes(HashMap<String, Album> albumes) {
+        this.albumes = albumes;
+    }
+
+    public void setListasDefecto(HashMap<String, ListaDefecto> listasDefecto) {
+        this.listasDefecto = listasDefecto;
+    }
+
+    public HashMap<String, Album> getAlbumes() {
+        return albumes;
+    }
+
+    public HashMap<String, ListaDefecto> getListasDefecto() {
+        return listasDefecto;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setSubgeneros(HashMap<String, Genero> subgeneros) {
+        this.subgeneros = subgeneros;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public HashMap<String, Genero> getSubgeneros() {
+        return subgeneros;
+    }
     
+    
+
 }
