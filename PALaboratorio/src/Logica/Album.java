539aad11868293dba2864d387e030f8c263917a9
Ale <Ale@192.168.56.1>
2017-08-09 22:36:16 -0300

    Merge origin/Desarrollo into Desarrollo
    Conflicts:
    	PALaboratorio/src/Logica/Artista.java
    	PALaboratorio/src/Logica/DtAlbum.java
    	PALaboratorio/src/Logica/DtAlbumContenido.java
    	PALaboratorio/src/Logica/DtArtista.java
    	PALaboratorio/src/Logica/DtCliente.java
    	PALaboratorio/src/Logica/DtFecha.java
    	PALaboratorio/src/Logica/DtGenero.java
    	PALaboratorio/src/Logica/DtPerfilArtista.java
    	PALaboratorio/src/Logica/DtPerfilCliente.java
    	PALaboratorio/src/Logica/DtPerfilUsuario.java
    	PALaboratorio/src/Logica/DtTema.java
    	PALaboratorio/src/Logica/DtUsuario.java

---
+++ PALaboratorio/src/Logica/Album.java
@@ -0,0 +1,69 @@
+package Logica;
+
+import java.util.ArrayList;
+
+class Album {
+
+    public String nombre;
+    public int anio;
+    public Imagen imagen;
+    public ArrayList<Tema> temas;
+    public ArrayList<Genero> generos;
+
+    public Album(String nombre, int anio, Imagen imagen, ArrayList<Tema> temas, ArrayList<Genero> generos) {
+        this.nombre = nombre;
+        this.anio = anio;
+        this.imagen = imagen;
+        this.temas = temas;
+        this.generos = generos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public Imagen getImagen() {
+        return imagen;
+    }
+
+    public ArrayList<Tema> getTemas() {
+        return temas;
+    }
+
+    public ArrayList<Genero> getGeneros() {
+        return generos;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public void setImagen(Imagen imagen) {
+        this.imagen = imagen;
+    }
+
+    public void setTemas(ArrayList<Tema> temas) {
+        this.temas = temas;
+    }
+
+    public void setGeneros(ArrayList<Genero> generos) {
+        this.generos = generos;
+    }
+
+    public DtAlbum getData() {
+     return null;
+    }
+
+    public DtAlbumContenido obtenerAlbumContenido() {
+     return null;
+    }
+
+}
