6e2cd7f4e1bdd6508b68e10f4ba7e8bb96492a77
Luis <Luis@Laptop>
2017-08-09 22:50:07 -0300

    mi primer commit eaea

--- PALaboratorio/src/Logica/ControladorUsuario.java
+++ PALaboratorio/src/Logica/ControladorUsuario.java
@@ -1,15 +1,41 @@
 package Logica;
 
+import java.util.List;
+import java.util.ArrayList;
+import Logica.Usuario;
+//import Persistencia.BDUsuario;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 import java.util.ArrayList;
 
 public class ControladorUsuario implements IUsuario{
 
+    private static ControladorUsuario instancia;
+    private Map<String, Usuario> usuarios;
+    
+    public static ControladorUsuario getInstance(){
+        if (instancia == null){
+            instancia = new ControladorUsuario();
+        }
+        return instancia;
+    }    
+
+    private ControladorUsuario(){
+        //Colección genérica común
+        //this.personas=new ArrayList<Persona>();
+        this.usuarios=new HashMap<String, Usuario>();
+        //this.dbPersona=new DBPersona();
+    }
+    
     public Artista selectArtista(String nick) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public void ingresarUsuario(DtUsuario dtu) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     public ArrayList<DtArtista> listarArtistas() {
